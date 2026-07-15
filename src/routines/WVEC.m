fa198d5312ef2204d59ef249d1db2f21517d4f18
Allan Steven Finkelstein <afinkelstein@yahoo.com>
2026-07-15 18:22:51 -0400

    Expand WVEC engineering console and integrate explorers

--- src/routines/WVEC.m
+++ src/routines/WVEC.m
@@ -1,29 +1,49 @@
 WVEC ; WorldVistA Engineering Console
- ;;1.1;WORLDVISTA ENGINEERING CONSOLE;;
+ ;;2.3;WORLDVISTA ENGINEERING CONSOLE;;
 
-MENU ;
+EN ; Entry Point
+ D MENU
+ Q
+
+MENU ; Main Menu
  N X
 
  F  D  Q:X=0
- . W @IOF
+ . W #
  . W !,"==============================================="
  . W !,"      WorldVistA Engineering Console"
  . W !,"==============================================="
  . W !
- . W !,"1  Engineering Doctor"
- . W !,"2  Status"
- . W !,"3  About"
- . W !,"0  Exit"
+ . W " Engineering"
+ . W !," -----------"
+ . W !," 1  Engineering Doctor"
+ . W !," 2  System Status"
+ . W !
+ . W " Explorers"
+ . W !," ---------"
+ . W !," 3  Global Explorer"
+ . W !," 4  Routine Explorer"
+ . W !," 5  KIDS Explorer"
+ . W !
+ . W " Information"
+ . W !," -----------"
+ . W !," 6  About"
+ . W !
+ . W !," 0  Exit"
  . W !
  . R "Select Option: ",X:300
  . S:'$T X=0
  . I X=1 D DOCTOR^WVECDOC Q
  . I X=2 D STATUS^WVECSTAT Q
- . I X=3 D ABOUT Q
+ . I X=3 D EN^WVECGLOB Q
+ . I X=4 D EN^WVECRTN Q
+ . I X=5 D EN^WVECKIDS Q
+ . I X=6 D ABOUT Q
  . I X'=0 W !,"Invalid selection." H 2
  Q
 
-ABOUT ;
+ABOUT ; Display version information
+ N X
  W !!
  W "WorldVistA Engineering Console (WVEC)",!
  W "Version: ",$$VERSION(),!
@@ -32,5 +52,5 @@ ABOUT ;
  R "Press RETURN to continue...",X
  Q
 
-VERSION() ;
- Q "1.1"
+VERSION() ; Return version string
+ Q "2.3-alpha"
