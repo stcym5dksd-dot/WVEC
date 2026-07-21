a73da7498d2cc3779f0c052307d9802cd11d2cad
Allan Steven Finkelstein <afinkelstein@yahoo.com>
2026-07-21 16:29:49 -0400

    Convert KIDS Explorer to WVECNAV provider architecture

--- src/routines/WVECKIDS.m
+++ src/routines/WVECKIDS.m
@@ -1,74 +1,110 @@
-WVECKIDS ; WVEC KIDS Explorer
- ;;3.1;WORLDVISTA ENGINEERING CONSOLE;;
+WVECKIDS ; WorldVistA KIDS Explorer Provider
+ ;;5.2;WORLDVISTA ENGINEERING CONSOLE;;
+ ;
  ;---------------------------------------------------------
  ; WorldVistA Engineering Console
  ;
- ; Routine:
- ;   WVECKIDS
+ ; KIDS Explorer Provider
  ;
- ; Purpose:
- ;   Read-only browser for the KIDS BUILD file (#9.6)
- ;   Displays the first 10 build names and the total
- ;   number of builds in the BUILD file.
+ ; Provider Interface
+ ;   EN
+ ;   TITLE()
+ ;   INIT()
+ ;   LIST()
+ ;   SELECT()
+ ;   UP()
+ ;   TOP()
  ;
- ; This routine makes NO changes to the database.
- ;
+ ; Allan S. Finkelstein
+ ; July 2026
  ;---------------------------------------------------------
  ;
+EN ; Launch KIDS Explorer
+ NEW ROOT
+ S ROOT=""
+ D START^WVECNAV("WVECKIDS",ROOT)
  Q
  ;
-EN ; Main Entry Point
+TITLE(CTX) ; Return screen title
+ I $G(CTX("MODE"))="MENU" Q "KIDS Build Menu"
+ Q "KIDS Explorer"
  ;
- N IEN,COUNT,NAME
+INIT(CTX) ; Initialize provider
+ K CTX("STACK")
+ S CTX("LEVEL")=0
+ S CTX("MODE")="LIST"
+ S CTX("PAGE")=1
+ K CTX("BUILD")
+ Q
+ ;
+LIST(CTX,LIST,COUNT) ; Build current list
+ NEW IEN,NAME,U
  S U="^"
- ;
- D BANNER^WVECUTIL("KIDS Explorer")
- ;
+
+ K LIST
  S COUNT=0
- S IEN=0
- ;
- F  S IEN=$O(^XPD(9.6,IEN)) Q:'IEN  D
- . S NAME=$P($G(^XPD(9.6,IEN,0)),U)
- . Q:NAME=""
- . S COUNT=COUNT+1
- . I COUNT'>10 W !,$J(COUNT,3),". ",NAME
- ;
- W !!
- W "Total Builds Found: ",COUNT
- W !!
- ;
- D PAUSE^WVECUTIL
+
+ I $G(CTX("MODE"))="LIST" D  Q
+ . S IEN=0
+ . F  S IEN=$O(^XPD(9.6,IEN)) Q:'IEN  D
+ . . S NAME=$P($G(^XPD(9.6,IEN,0)),U)
+ . . Q:NAME=""
+ . . S COUNT=COUNT+1
+ . . S LIST(COUNT)=NAME
+
+ I $G(CTX("MODE"))="MENU" D
+ . S COUNT=4
+ . S LIST(1)="Header"
+ . S LIST(2)="Required Builds"
+ . S LIST(3)="Package File"
+ . S LIST(4)="Back"
  Q
  ;
-LIST(MAX) ; Display the first MAX build names
- ;
- N IEN,COUNT,NAME,LIMIT
- S U="^"
- ;
- S LIMIT=$S($G(MAX)>0:MAX,1:10)
- S COUNT=0
- S IEN=0
- ;
- F  S IEN=$O(^XPD(9.6,IEN)) Q:'IEN  D  Q:COUNT'<LIMIT
- . S NAME=$P($G(^XPD(9.6,IEN,0)),U)
- . Q:NAME=""
- . S COUNT=COUNT+1
- . W !,$J(COUNT,3),". ",NAME
- ;
+SELECT(CTX,ITEM) ; Handle selection
+
+ I $G(CTX("MODE"))="LIST" D  Q
+ . S CTX("BUILD")=ITEM
+ . S CTX("MODE")="MENU"
+ . D PUSH^WVECNAV(.CTX,ITEM)
+
+ I ITEM="Back" D  Q
+ . D POP^WVECNAV(.CTX)
+ . S CTX("MODE")="LIST"
+
+ I ITEM="Header" D  Q
+ . N IEN
+ . S IEN=$O(^XPD(9.6,"B",CTX("BUILD"),0))
+ . W !!
+ . W "Build: ",CTX("BUILD"),!
+ . I IEN>0 W $G(^XPD(9.6,IEN,0)),!
+ . R !!,"Press ENTER to continue...",X
+
+ I ITEM="Required Builds" D  Q
+ . W !!
+ . W "Required Build display not yet implemented."
+ . R !!,"Press ENTER to continue...",X
+
+ I ITEM="Package File" D  Q
+ . W !!
+ . W "Package information not yet implemented."
+ . R !!,"Press ENTER to continue...",X
+
  Q
  ;
-COUNT() ; Return total number of builds
+UP(CTX) ; Navigate up
+ I $G(CTX("MODE"))="MENU" D
+ . D POP^WVECNAV(.CTX)
+ . S CTX("MODE")="LIST"
+ Q
  ;
- N IEN,TOTAL
+TOP(CTX) ; Return to top
+ K CTX("STACK")
+ S CTX("LEVEL")=0
+ S CTX("MODE")="LIST"
+ K CTX("BUILD")
+ S CTX("PAGE")=1
+ Q
  ;
- S TOTAL=0
- S IEN=0
- ;
- F  S IEN=$O(^XPD(9.6,IEN)) Q:'IEN  S TOTAL=TOTAL+1
- ;
- Q TOTAL
- ;
-VERSION() ; Return WVEC module version
- ;
- Q "1.0"
+VERSION() ;
+ Q "5.2"
  ;
